c9c4d89ba0cfc7fbf1fe089c6d02fb3484ed59a7
T1406 <117759979+T1406@users.noreply.github.com>
2022-11-08 23:55:48 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: "Tớ xin lỗi mà :(( DDA tha lỗi cho tớ nhá.-.",
   descriptionHeader:
     "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
-  buttonYes: "Đồng Ý",
-  buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn em vì đồng ý",
+  buttonYes: "Click here to forgive dtt",
+  buttonNo: "Không!?",
+  titleModar: "Xin loi dda mot lan nua vi cot nha luc cau dang met :<",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    'Arigatooooo ^^ </br> Cam on nha ^^ khi nao xuong HP cau muon to lam gi cung duoc ay  </br> "Arigatooooo </br> Arigatooooo "',
 };
